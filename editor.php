b13e0da4e9f487bf95b423ce5616c78f86b28287
jairpro <jairedias@yahoo.com.br>
2020-02-16 17:39:49 -0300

    Compilação dos últimos commits
    ATENÇÃO:
    Comentei toda a criação de LEVELS original da game.js.
    A partir de agora ele lê diretamente dos arquivos da pasta:
    
    ./editor/levels/game/
    
    Os arquivos desta pasta foram atualizados com a última revisão de LEVELS do repositório master até esse momento.
    
    Caso seja preciso chamar loadLevel() sem PHP basta informar na query da URL o valor nophp, exemplos:
    
    127.0.0.1:5500?nophp
    
    http://joaolucas.xyz/game/?nophp
    
    ##Prevenindo uso de mais intancias de Editor##
    
    ##Iniciando com arquivos da pasta levels/game##
    
    ##Iniciando com arquivos da pasta levels/game (completando comit anterior)##
    
    ##Tentando resolver conflito de versões##
    Adicionei novamente a criação de LEVELS porém deixei todo comentado
    
    ##Corrigindo: Adicionei novamente a criação original de LEVELS todo comentado.##
    
    #Edição de arquivos de LEVELs e Joypad#
    
    ##Edição de arquivos de Levels (requer PHP):##
    
    Alterao alguns atalhos (revisar no código, entre eles CTRL+ENTER para ativar/desativar o editor);
    
    Navegações de levels (CTRL+RIGHT e CTRL+LEFT);
    
    Sugestão para adicionar novo level ao chegar aos bordos da navegação de levels (ATENÇÃO: tem um erro conhecido que após adicionar não seleciona o novo level, precisando voltar);
    
    Salvar level (CTRL+X , like nano :=)
    
    Inserir novo level entre os levels (CTRL+INS);
    
    Excluir level com confirmação (CTRL+DEL);
    
    SHIFT+ENTER: alterna uso de direcionais do editor (setas ou teclas WDSA);
    
    adicionado e.preventDefaul() para todos os atalhos de teclado;
    
    ###Joypad funcional para o game e o editor (falta adicionar mais comando via joypad)###

--- editor.php
+++ editor.php
@@ -10,20 +10,41 @@
 
     public $items;
 
+    function updateFiles() {
+      if (is_array($this->items)) {
+        foreach ($this->items as $i=>$item) {
+          $path = isset($item["path"]) ? $item["path"] : false;
+          if (is_string($path) && is_dir($path) && file_exists($path)) {
+            $files = scandir($path);
+            if (is_array($files)) {
+              $this->items[$i]["files"] = array();
+              foreach ($files as $f=>$file) {
+                if (!in_array($file, array(".",".."))) {
+                  unset($this->items[$i]["length"]);
+                  $this->items[$i]["files"][] = $file;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
     function load() {
       $ok = false;
       if (file_exists(self::path)) {
         $content = file_get_contents(self::path);
         if (is_string($content)) {
           $this->items = json_decode($content, true);
+          $this->updateFiles();
           $ok = true;
         }
       }
       return $ok;
     }
 
-    function save() {
-      $content = json_encode($this->items);
+    function save($content) {
+      $content = $content ? $content : json_encode($this->items);
       $size = file_put_contents(self::path, $content);
       return $size!==false && $size === strlen($content);
     }
@@ -56,16 +77,83 @@
       return false;
     }
 
+    function loadLevels($gameId) {
+
+      function callback($linha) {
+        return substr(substr($linha, 1), 0, -1);
+      }
+  
+      $game = $this->getGameById($gameId);
+      $levels = array();
+      if (isset($game['files']) && is_array($game['files'])) {
+        foreach($game['files'] as $f=>$file) {
+          if (isset($game['path'])) {
+            $path = $game['path'] . (substr($game['path'],-1,0)!=="/" ? "/" : "") . $file;
+            if (is_file($path) && file_exists($path)) {
+              $content = file_get_contents($path);
+              if ($content!==false) {
+                $level = explode(PHP_EOL, $content);
+                $levels[] = array_map("callback", $level);
+              }
+              else {
+                return false;
+              }
+            }
+            else {
+              return false;
+            }
+          }
+          else {
+            return false;
+          }
+        }
+      }
+      else {
+        return false;
+      }
+      return $levels;
+    }
+
+    function deleteLevel($gameId, $levelFile) {
+      $ok = false;
+      $game = $this->getGameById($gameId);
+      if (isset($game["path"])) {
+        $levelPath = $game["path"] . (substr($game["path"],-1,1)==="/" ? "" : "/") . $levelFile;
+        if (is_file($levelPath) && file_exists($levelPath)) {
+          $ok = unlink($levelPath);
+        }
+      }
+      return $ok;
+    }
+
   }
 
   $ok = false;
   $message = "";
-  $content = null;
+  $content = "";
   
   try {
     $function = filter_input(INPUT_POST, "function");
     
-    if ($function==="saveLevel") {
+    if ($function==="loadGames") {
+      $games = new Games();
+      $ok = $games->load();
+      if (!$ok) {
+        $message = "Error on load games file";
+      }
+      else {
+        $content = json_encode(array_values($games->items), true);
+      }
+    }
+    else if ($function==="saveGames") {
+      $gamesContent = filter_input(INPUT_POST, "games");
+      $games = new Games();
+      $ok = $games->save($gamesContent);
+      if (!$ok) {
+        $message = "Error on save games file";
+      }
+    }
+    else if ($function==="saveLevel") {
       $gameId = filter_input(INPUT_POST, "gameId");
       $gameFolder = filter_input(INPUT_POST, "gameFolder");
       $levelNumber = filter_input(INPUT_POST, "levelNumber");
@@ -90,14 +178,37 @@
         $message = "Erro ao carregar level";
       }
     }
+    else if ($function==="loadLevels") {
+      $gameId = filter_input(INPUT_POST, "gameId");
+      $games = new Games();
+      $games->load();
+      $levels = $games->loadLevels($gameId);
+      $ok = is_array($levels);
+      if (!$ok) {
+        $message = "Error on load levels";
+      }
+      else {
+        $content = $levels;
+      }     
+    }
+    else if ($function==="deleteLevel") {
+      $gameId = filter_input(INPUT_POST, "gameId");
+      $levelFile = filter_input(INPUT_POST, "levelFile");
+      $games = new Games();
+      $games->load();
+      $ok = $games->deleteLevel($gameId, $levelFile);
+      if (!$ok) {
+        $message = "Error on delete level file";
+      }
+    }
     else {
-      $message = "ivalid function";
+      $message = "invalid function";
     }
   }
   catch (Exception $ex) {
     $message = $ex->getMessage();
   }
-
-  $result = array("ok"=>$ok, "content"=>$content, "message"=>$message, "ouput"=>ob_get_clean());
+  $output = ob_get_clean();
+  $result = array("ok"=>$ok, "content"=>$content, "message"=>$message, "ouput"=>$output);
   $json = json_encode($result);
   echo $json;
